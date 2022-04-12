b7a3dd9dea6e25e8ffda8ef245694f01292711d1
Milko Yanakiev <myanakiev@degiro.nl>
2022-04-12 12:15:12 +0300

    Final.

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM openjdk:8-jdk-alpine
 RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} spring-boot-complete-0.0.1.jar
-ENTRYPOINT ["java","-jar","/spring-boot-complete-0.0.1.jar"]
-
+COPY ${JAR_FILE} spring-boot-complete-0.0.2.jar
+ENTRYPOINT ["java","-jar","/spring-boot-complete-0.0.2.jar"]
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,14 +8,14 @@ import java.util.UUID;
 
 @RestController
 public class HelloController {
-	
+
 	private static final String uid = UUID.randomUUID().toString();
 
 	private static int count = 0;
 
 	@GetMapping("/")
 	public String index() throws InterruptedException {
-		return ++count + " Greetings from ICS! </br> InstanceID = " + uid;
+		return "<h1>Version 2</h1></br>" + ++count + " Greetings from ICS! </br> InstanceID = " + uid;
 	}
 
 }
